585c39abe61a64be054de98bd1e403849d0011c9
grcamis <140463743+grcamis@users.noreply.github.com>
2023-09-08 22:55:58 +0000

    atualizando jogo da velha

--- main.rs
+++ main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-fn imprimeTabuleiro(tab: Vec<Vec<String>>) {
+fn imprimeTabuleiro(tab: &Vec<Vec<String>>) {
     for i in 0..3 {
         for j in 0..3 {
             print!(" {} ", tab[i][j]);
@@ -9,161 +9,135 @@ fn imprimeTabuleiro(tab: Vec<Vec<String>>) {
     }
 }
 
-fn checaLinhaInclinada(tabuleiro: Vec<Vec<String>>, opcaoEscolhida:&str) -> bool {
-    if tabuleiro[0][0] == opcaoEscolhida.trim().to_string() && tabuleiro[1][1] == opcaoEscolhida.trim().to_string() && tabuleiro[2][2] == opcaoEscolhida.trim().to_string() {
-        return true;
+fn geraNumero() -> i32 {
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Falha ao ler a linha");
+
+        match input.trim().parse::<i32>() {
+            Ok(value) if value >= 0 && value <= 2 => return value,
+            _ => println!("Por favor, insira um número entre 0 e 2."),
+        }
     }
+}
+
+fn verificaCasa(tabuleiro: &mut Vec<Vec<String>>, opcaoEscolhida: &str) {
+    let mut verificaJogada = false;
+    let mut numeroLinha: i32;
+    let mut numeroColuna: i32;
+
+    while verificaJogada == false {
+        println!("Jogador {opcaoEscolhida} por favor escolha a linha");
+        numeroLinha = geraNumero();
+        println!("Jogador {opcaoEscolhida} por favor escolha a coluna");
+        numeroColuna = geraNumero();
+        if tabuleiro[numeroLinha as usize][numeroColuna as usize] == "_" {
+            tabuleiro[numeroLinha as usize][numeroColuna as usize] =
+                opcaoEscolhida.trim().to_string();
+            imprimeTabuleiro(&tabuleiro);
+            verificaJogada = true;
+        } else {
+            println!("Ponto já utilizado, por favor escolha outros");
+        }
+    }
+}
+
+fn checaJogadas(tabuleiro: &Vec<Vec<String>>, opcaoEscolhida: &str) -> bool {
+    for i in 0..3 {
+        let mut vitoriaVertical: i32 = 0;
+        for j in 0..3 {
+            if tabuleiro[i][j] != opcaoEscolhida {
+                break;
+            } else {
+                vitoriaVertical += 1;
+            }
+        }
+        if vitoriaVertical == 3 {
+            println!("{opcaoEscolhida} venceu!");
+            return true;
+        }
+    }
+
+    for i in 0..3 {
+        let mut vitoriaHorizontal: i32 = 0;
+        for j in 0..3 {
+            if tabuleiro[j][i] != opcaoEscolhida {
+                break;
+            } else {
+                vitoriaHorizontal += 1;
+            }
+        }
+        if vitoriaHorizontal == 3 {
+            println!("{opcaoEscolhida} venceu!");
+            return true;
+        }
+    }
+
     return false;
 }
 
-fn checaLinhaInclinada2(tabuleiro: Vec<Vec<String>>, opcaoEscolhida:&str) -> bool {
-    if tabuleiro[0][0] == opcaoEscolhida.trim().to_string() && tabuleiro[1][0] == opcaoEscolhida.trim().to_string() && tabuleiro[2][0] == opcaoEscolhida.trim().to_string() {
-        return true;
-    }
-    return false;
-}
-
-fn checaLinhaHorizontalEsquerda(tabuleiro: Vec<Vec<String>>, opcaoEscolhida:&str) -> bool {
-    if tabuleiro[0][2] == opcaoEscolhida.trim().to_string() && tabuleiro[1][1] == opcaoEscolhida.trim().to_string() && tabuleiro[2][0] == opcaoEscolhida.trim().to_string() {
-        return true;
-    }
-    return false;
-}
-
-fn checaLinhaVerticalCima(tabuleiro: Vec<Vec<String>>, opcaoEscolhida:&str) -> bool {
-    if tabuleiro[0][0] == opcaoEscolhida.trim().to_string() && tabuleiro[0][1] == opcaoEscolhida.trim().to_string() && tabuleiro[0][2] == opcaoEscolhida.trim().to_string() {
-        return true;
-    }
-    return false;
-}
-
-fn checaLinhaHorizontalDireita(tabuleiro: Vec<Vec<String>>, opcaoEscolhida:&str) -> bool {
-    if tabuleiro[0][2] == opcaoEscolhida.trim().to_string() && tabuleiro[1][2] == opcaoEscolhida.trim().to_string() && tabuleiro[2][2] == opcaoEscolhida.trim().to_string() {
-        return true;
-    }
-    return false;
-}
-
-fn checaLinhaVerticalBaixo(tabuleiro: Vec<Vec<String>>, opcaoEscolhida:&str) -> bool {
-    if tabuleiro[2][0] == opcaoEscolhida.trim().to_string() && tabuleiro[2][1] == opcaoEscolhida.trim().to_string() && tabuleiro[2][2] == opcaoEscolhida.trim().to_string() {
-        return true;
-    }
-    return false;
-}
-
-fn checaLinhaHorizontalMeio(tabuleiro: Vec<Vec<String>>, opcaoEscolhida:&str) -> bool {
-    if tabuleiro[0][1] == opcaoEscolhida.trim().to_string() && tabuleiro[1][1] == opcaoEscolhida.trim().to_string() && tabuleiro[2][1] == opcaoEscolhida.trim().to_string() {
-        return true;
-    }
-    return false;
-}
-
-fn verificaEmpate(tabuleiro: Vec<Vec<String>>) -> bool {
+fn verificaEmpate(tabuleiro: &Vec<Vec<String>>) -> bool {
     for i in 0..3 {
         for j in 0..3 {
-            if(tabuleiro[i][j]) == "_" {
+            if (tabuleiro[i][j]) == "_" {
                 return false;
             }
         }
     }
 
+    println!("Empate!");
     return true;
+}
 
+fn le_linha() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Falha ao ler a linha");
+    input.trim().to_string()
 }
 
 fn main() {
-   println! ("Participante 1 escolha X ou O");
-   let mut opcaoEscolhida = String::new();
-   io::stdin().read_line(&mut opcaoEscolhida).expect("Falha ao ler a opcao");
+    println!("Participante 1 escolha X ou O");
+    let mut opcaoEscolhida = le_linha();
 
-   while opcaoEscolhida.trim() != "X" && opcaoEscolhida.trim() != "O" {
-       println! ("Digite novamente");
-       opcaoEscolhida = String::new();
-       io::stdin().read_line(&mut opcaoEscolhida).expect("Falha ao ler a opcao");
-}
+    while opcaoEscolhida.trim() != "X" && opcaoEscolhida.trim() != "O" {
+        println!("Digite novamente");
+        opcaoEscolhida = le_linha();
+    }
 
-   println! ("Voce escolheu a opcao {opcaoEscolhida}");
+    println!("Voce escolheu a opcao {opcaoEscolhida}");
 
-   let opcaoEscolhida2;
-   if opcaoEscolhida.trim() == "X" {
-       opcaoEscolhida2 = "O";
-
+    let opcaoEscolhida2;
+    if opcaoEscolhida.trim() == "X" {
+        opcaoEscolhida2 = "O";
     } else {
         opcaoEscolhida2 = "X";
+    }
+
+    println!("Participante 2 sera a opcao {opcaoEscolhida2}");
+
+    let mut tabuleiro: Vec<Vec<String>> = vec![vec!["_".to_string(); 3]; 3];
+
+    loop {
+        if verificaEmpate(&tabuleiro) {
+            break;
         }
 
-        println! ("Participante 2 sera a opcao {opcaoEscolhida2}");
-
-        let mut tabuleiro: Vec<Vec<String>> = vec![vec!["_".to_string();3];3];
-
-        let mut jogoRodando = true;
-
-        while jogoRodando == true {
-
-        let mut verificaJogada = false;
-
-        if verificaEmpate(tabuleiro.clone()) {
-            jogoRodando = false;
-            println!("Empate!")
+        verificaCasa(&mut tabuleiro, opcaoEscolhida.as_str().trim());
+        if checaJogadas(&tabuleiro, opcaoEscolhida.as_str().trim()) {
+            break;
         }
 
-        if jogoRodando == true {
-        while verificaJogada == false {
-
-        println!("Primeiro jogador escolha a linha: ");
-        let mut linhaEscolhida = String::new();
-        io::stdin().read_line(&mut linhaEscolhida).expect("Falha ao ler a opcao");
-        let numeroLinha :usize = linhaEscolhida.trim().parse().expect("Falha ao ler o numero");
-        println!("Primeiro jogador escolha a coluna: ");
-        let mut colunaEscolhida = String::new();
-        io::stdin().read_line(&mut colunaEscolhida).expect("Falha ao ler a opcao");
-        let numeroColuna :usize = colunaEscolhida.trim().parse().expect("Falha ao ler o numero");
-
-        if tabuleiro[numeroLinha][numeroColuna] == "_" {
-            tabuleiro[numeroLinha][numeroColuna] = opcaoEscolhida.trim().to_string();
-            imprimeTabuleiro(tabuleiro.clone());
-            verificaJogada = true;
+        if verificaEmpate(&tabuleiro) {
+            break;
         }
 
-        if checaLinhaInclinada(tabuleiro.clone(),opcaoEscolhida.trim()) || checaLinhaInclinada2(tabuleiro.clone(),opcaoEscolhida.trim()) || checaLinhaHorizontalEsquerda(tabuleiro.clone(),opcaoEscolhida.trim()) || checaLinhaVerticalCima(tabuleiro.clone(),opcaoEscolhida.trim()) || checaLinhaHorizontalDireita(tabuleiro.clone(),opcaoEscolhida.trim()) || checaLinhaVerticalBaixo(tabuleiro.clone(),opcaoEscolhida.trim()) || checaLinhaHorizontalMeio(tabuleiro.clone(),opcaoEscolhida.trim()) {
-            println!("O Jogador um venceu!");
-            jogoRodando = false;
+        verificaCasa(&mut tabuleiro, opcaoEscolhida2);
+        if checaJogadas(&tabuleiro, opcaoEscolhida2) {
+            break;
         }
-
-        }
-
-        }
-
-        if verificaEmpate(tabuleiro.clone()) {
-            jogoRodando = false;
-            println!("Empate!");
-        }
-
-        if jogoRodando == true {
-        verificaJogada = false;
-        while verificaJogada == false {
-        println!("Segundo jogador escolha a linha: ");
-        let mut linhaEscolhida = String::new();
-        io::stdin().read_line(&mut linhaEscolhida).expect("Falha ao ler a opcao");
-        let numeroLinha :usize = linhaEscolhida.trim().parse().expect("Falha ao ler o numero");
-        println!("Segundo jogador escolha a coluna: ");
-        let mut colunaEscolhida = String::new();
-        io::stdin().read_line(&mut colunaEscolhida).expect("Falha ao ler a opcao");
-        let numeroColuna :usize = colunaEscolhida.trim().parse().expect("Falha ao ler o numero");
-                if tabuleiro[numeroLinha][numeroColuna] == "_" {
-            tabuleiro[numeroLinha][numeroColuna] = opcaoEscolhida2.trim().to_string();
-            imprimeTabuleiro(tabuleiro.clone());
-            verificaJogada = true;
-        }
-
-                if checaLinhaInclinada(tabuleiro.clone(),opcaoEscolhida2.trim()) || checaLinhaInclinada2(tabuleiro.clone(),opcaoEscolhida2.trim()) || checaLinhaHorizontalEsquerda(tabuleiro.clone(),opcaoEscolhida2.trim()) || checaLinhaVerticalCima(tabuleiro.clone(),opcaoEscolhida2.trim()) || checaLinhaHorizontalDireita(tabuleiro.clone(),opcaoEscolhida2.trim()) || checaLinhaVerticalBaixo(tabuleiro.clone(),opcaoEscolhida2.trim()) || checaLinhaHorizontalMeio(tabuleiro.clone(),opcaoEscolhida2.trim()) {
-                    println!("O Jogador dois venceu!");
-                    jogoRodando = false;
-                    break;
-        }
-
-        }
-        }
-        }
-
+    }
 }
